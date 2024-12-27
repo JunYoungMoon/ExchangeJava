955af58e294707077aa29fa6b9a8e6731a14d617
moon <ko5642027@naver.com>
2024-12-27 23:48:51 +0900

    - 일일 종가 배치 처리 제외 - 배치는 추후에 사용, 현재는 총금액과 수량 정도만 계산하도록 처리

--- Coin/src/main/java/com/mjy/coin/batch/schedule/BatchScheduler.java
+++ Coin/src/main/java/com/mjy/coin/batch/schedule/BatchScheduler.java
@@ -18,43 +18,43 @@ import org.springframework.stereotype.Component;
 public class BatchScheduler {
 
     private final JobLauncher jobLauncher;
-    private final Job coinOrderJob;
-//    private final Job redisToMysqlJob;
+//    private final Job coinOrderJob;
+    private final Job redisToMysqlJob;
 
     @Autowired
     public BatchScheduler(JobLauncher jobLauncher,
-                          @Qualifier("coinOrderJob") Job coinOrderJob/*,
-                          /@Qualifier("redisToMysqlJob") Job redisToMysqlJob*/) {
+//                          @Qualifier("coinOrderJob") Job coinOrderJob,
+                          @Qualifier("redisToMysqlJob") Job redisToMysqlJob) {
         this.jobLauncher = jobLauncher;
-        this.coinOrderJob = coinOrderJob;
-//        this.redisToMysqlJob = redisToMysqlJob;
+//        this.coinOrderJob = coinOrderJob;
+        this.redisToMysqlJob = redisToMysqlJob;
     }
 
-    @Bean
-    public ApplicationRunner runAtStartup() {
-        return args -> runCoinOrderJob();
-    }
-
-    @Scheduled(cron = "0 0 2 * * ?") // 매일 오전 2시에 실행
-    public void runCoinOrderJob() {
-        try {
-            jobLauncher.run(coinOrderJob, new JobParametersBuilder()
-                    .addLong("run.id", System.currentTimeMillis())
-                    .toJobParameters());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-//    @Scheduled(fixedRate = 60000 * 5) // 5분마다 실행
-//    public void runRedisToMysqlJob() {
+//    @Bean
+//    public ApplicationRunner runAtStartup() {
+//        return args -> runCoinOrderJob();
+//    }
+//
+//    @Scheduled(cron = "0 0 2 * * ?") // 매일 오전 2시에 실행
+//    public void runCoinOrderJob() {
 //        try {
-//            jobLauncher.run(redisToMysqlJob, new JobParametersBuilder()
+//            jobLauncher.run(coinOrderJob, new JobParametersBuilder()
 //                    .addLong("run.id", System.currentTimeMillis())
 //                    .toJobParameters());
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
 //    }
+
+
+    @Scheduled(fixedRate = 60000 * 5) // 5분마다 실행
+    public void runRedisToMysqlJob() {
+        try {
+            jobLauncher.run(redisToMysqlJob, new JobParametersBuilder()
+                    .addLong("run.id", System.currentTimeMillis())
+                    .toJobParameters());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
